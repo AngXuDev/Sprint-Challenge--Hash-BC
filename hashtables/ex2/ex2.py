7c1c50eba9bad2619335ddd330b58e43bb2c17af
Ang Xu <angxu1234@gmail.com>
2019-11-01 11:49:33 -0500

    ex2 hash complete

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,17 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    # Build hash table by inserting the source as key and destination as value
+    for i in range(length):
+        start = tickets[i].source
+        end = tickets[i].destination
+        hash_table_insert(hashtable, start, end)
+
+    # create initial element of route list by grabbing the ticket with NONE key/source
+    route[0] = hash_table_retrieve(hashtable, 'NONE')
+    # loop and add tickets to the route by using the destination/value of prior ticket as
+    # the key/source for the current ticket
+    for i in range(1, length):
+        route[i] = hash_table_retrieve(hashtable, route[i-1])
 
     return route
